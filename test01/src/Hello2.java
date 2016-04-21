f4c36a965b58d429e584b1e03ef394bc0966db2b
goodbyh <greatguy.bae@gmail.com>
2016-04-21 10:33:59 +0900

    f

--- test01/src/Hello2.java
+++ test01/src/Hello2.java
@@ -3,6 +3,7 @@ public class Hello2 {
   public static void main(String[] args) {
     System.out.println("^_^");
     System.out.println("T_T");
+    System.out.println("헬로투 편집하세요");
     System.out.println("천배");
  // 으아아아아아아아앙ㅇㅇ아ㅏㅏ아ㅏ아아ㅏ아
   }
